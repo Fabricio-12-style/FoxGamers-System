d9409faa7e7c1cd3175fbdc54b5a8d3fa2dda043
Fabricio-12-style <fabriciojaircarlobernuy@gmail.com>
2026-09-22 23:45:00 -0500

    Corregir CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,8 @@ const empresaRoutes = require("./routes/empresaRoutes");
 const reporteRoutes = require("./routes/reporteRoutes");
 
 // Middlewares
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"].filter(Boolean);
+const frontendUrl = process.env.FRONTEND_URL?.replace(/\/$/, "");
+const allowedOrigins = [frontendUrl, "http://localhost:5173"].filter(Boolean);
 
 app.use(
   cors({
@@ -31,6 +32,7 @@ app.use(
 
       return callback(new Error("Origen no permitido por CORS"));
     },
+    credentials: true,
   }),
 );
 app.use(express.json({ limit: "50mb" }));
